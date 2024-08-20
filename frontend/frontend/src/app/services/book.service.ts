b820cbd4a57afddd6c788df2dae4fa4009ce5377
vinicius <vinicius.suporte@pro4ce.com>
2024-08-20 17:30:31 -0300

    commit inicial

---
+++ frontend/frontend/src/app/services/book.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'https://bookmanager-backend.herokuapp.com/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+}
